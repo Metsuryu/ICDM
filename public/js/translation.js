7af48c4eee6877e0d3d3250a5063b42a517a9efe
Mario Cannistrà <Metsuryu@users.noreply.github.com>
2017-04-23 22:05:41 +0200

    Translate Login page

--- public/js/translation.js
+++ public/js/translation.js
@@ -1,18 +1,50 @@
+//TODO: Make separate translation file for home page.
+let localLanguage = "Eng";
+
 function toEn () {
+	console.log("To ENG");
 	//TODO:
 	Cookies.set("Lang", "Eng");
-	//Cookies.get("Lang");
+	//Header bar
+	$("#langFlag").attr("src","img/flags/us.gif");
+	$("#aboutusLabel").text("About us");
+	$("#fbGroupLabel").text("Facebook group");
+	$("#langLabel").text("Language");
+	$("#contLabel").text("Contact us");
+	//Body
+	$("#bgLogo").attr("src","/img/underwaterLogoENG.png");
+	$("#loginLabel").text("Login or Register with:");
+	$("#ppolLabel").text("Privacy policy");
+	$("#tosLabel").text("Terms of service");
 }
 
-
-
 function toIt () {
+	console.log("To ITA");
 	//TODO:
 	Cookies.set("Lang", "Ita");
-	//Cookies.get("Lang");
+	//Header bar
+	$("#langFlag").attr("src","img/flags/it.gif");
+	$("#aboutusLabel").text("Chi siamo");
+	$("#fbGroupLabel").text("Gruppo Facebook");
+	$("#langLabel").text("Lingua");
+	$("#contLabel").text("Contattaci");
+	//Body
+	$("#bgLogo").attr("src","/img/underwaterLogoITA.png");
+	$("#loginLabel").text("Entra o Registrati con:");
+	$("#ppolLabel").text("Informativa sulla privacy");
+	$("#tosLabel").text("Termini e condizioni");
 }
 
 $(document).ready(function(){
+	//Detect cookie language, and translate if italian
+	localLanguage = Cookies.get("Lang");
+	/*If the cookie is set to "Ita", translate the page to Italian, 
+	in any other case, keep it in english.*/
+	if (localLanguage === "Ita") {
+		toIt();
+	};
+
+
 	$("#langSelector").click(function(){
 		if ( $( "#langSelect" ).length ) {
 			$( "#langSelect" ).remove();
