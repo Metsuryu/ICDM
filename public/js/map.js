46959660b4dbbf8db44dc0dc85d10265e2d9fcc0
Mario Cannistrà <Metsuryu@users.noreply.github.com>
2017-05-04 10:04:04 +0200

    Babel compiled and updateMarkers change
    updateMarkersOnMap now is called at start of application, and as soon as
    the position is broadcasted (But it doesn't seem to have any significant
    effect)

--- public/js/map.js
+++ public/js/map.js
@@ -1,38 +1,40 @@
-let map;
+"use strict";
+
+var map = void 0;
 //Italy: lat: 41.500, lng: 13.304 
-const defaultLat = 41.500;
-const defaultLng = 13.304;
-let usersOnline = [];
-let markersOnMap = [];
+var defaultLat = 41.500;
+var defaultLng = 13.304;
+var usersOnline = [];
+var markersOnMap = [];
 //If localUserOnMap === true, update map position when doing broadcast
-let localUserOnMap = false;
+var localUserOnMap = false;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: defaultLat, lng: defaultLng}, 
-          zoom: 5
-          /*
-          1: World
-          5: Landmass/continent
-          10: City
-          15: Streets
-          20: Buildings
-          */
-        });
+    center: { lat: defaultLat, lng: defaultLng },
+    zoom: 5
+    /*
+    1: World
+    5: Landmass/continent
+    10: City
+    15: Streets
+    20: Buildings
+    */
+  });
 }
 
 function showMessageOnMap(message, pos) {
   if (!pos) {
     pos = map.getCenter();
   };
-  let infoWindow = new google.maps.InfoWindow({map: map});
+  var infoWindow = new google.maps.InfoWindow({ map: map });
   infoWindow.setPosition(pos);
   infoWindow.setContent(message);
 }
 
 function handleLocationError(browserHasGeolocation, pos) {
-  let geoFail = "Error: The Geolocation service failed.";
-  let geoNotSup = "Error: Your browser doesn't support geolocation.";
+  var geoFail = "Error: The Geolocation service failed.";
+  var geoNotSup = "Error: Your browser doesn't support geolocation.";
   if (localLanguage === "Ita") {
     geoFail = "Errore: Il servizio di geolocazione non ha funzionato.";
     geoNotSup = "Errore: Il tuo browser non supporta la geolocazione.";
@@ -41,59 +43,58 @@ function handleLocationError(browserHasGeolocation, pos) {
   showMessageOnMap(browserHasGeolocation ? geoFail : geoNotSup, pos);
 }
 
-function emitUpdateCoords (newLat, newLng){
-  let socket = io();
-    if (sessionID) {
-      socket.emit("updateOnlineContactCoords", {
-        uniqueID: user._id,
-        lat: newLat,
-        lng: newLng
-      });
-    }else{
-      //console.log("Error: Could not retrive ID");
-      let errorMsg = "Error: Could not retrive ID from server. Try refreshing the page.";
-      if (localLanguage === "Ita") {
-        errorMsg = "Errore: Impossibile ricevere ID dal server. Provare a ricaricare la pagina.";
-      }
-      showMessageOnMap(errorMsg)
-    };
+function emitUpdateCoords(newLat, newLng) {
+  var socket = io();
+  if (sessionID) {
+    socket.emit("updateOnlineContactCoords", {
+      uniqueID: user._id,
+      lat: newLat,
+      lng: newLng
+    });
+  } else {
+    //console.log("Error: Could not retrive ID");
+    var errorMsg = "Error: Could not retrive ID from server. Try refreshing the page.";
+    if (localLanguage === "Ita") {
+      errorMsg = "Errore: Impossibile ricevere ID dal server. Provare a ricaricare la pagina.";
+    }
+    showMessageOnMap(errorMsg);
+  };
 }
 
 //Sets also user's location to 0, 0 and removes the marker from map
-function eraseLocation(){
+function eraseLocation() {
   $.ajax({
     type: "PUT",
     url: "/updateUserCoordinates",
     data: {
       userID: user._id,
-      lat: 0, 
+      lat: 0,
       lng: 0
     },
-    success: function() {
+    success: function success() {
       //console.log("Location Erased");
       emitUpdateCoords(0, 0);
       user.lat = 0;
       user.lng = 0;
       removeLocalUserMarker();
     },
-    error: function(err){
-      console.log("Error: " , err);
+    error: function error(err) {
+      console.log("Error: ", err);
     },
-    complete: function(){
-    }
+    complete: function complete() {}
   });
 }
 
 //Get current location of the user, and broadcast them to the server. 
-function broadcastLocation(){
+function broadcastLocation() {
   //If user's marker is already on map, erase it first, so it can spawn at new location.
   if (localUserOnMap) {
     eraseLocation();
   };
   // Try HTML5 geolocation. 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      let pos = {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
@@ -104,168 +105,170 @@ function broadcastLocation(){
         url: "/updateUserCoordinates",
         data: {
           userID: user._id,
-          lat: pos.lat, 
+          lat: pos.lat,
           lng: pos.lng
         },
-        success: function() {
+        success: function success() {
           //console.log("Coordinates Updated.");
           emitUpdateCoords(pos.lat, pos.lng);
           user.lat = pos.lat;
           user.lng = pos.lng;
-          updateMarkersOnMap ();
+          //updateMarkersOnMap();
         },
-        error: function(err){
-          console.log("Error: " , err);
+        error: function error(err) {
+          console.log("Error: ", err);
         },
-        complete: function(){
+        complete: function complete() {
+          updateMarkersOnMap();
         }
       });
-
-    }, function() {
+    }, function () {
       handleLocationError(true, map.getCenter());
     });
   } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, map.getCenter());
-    }
+    // Browser doesn't support Geolocation
+    handleLocationError(false, map.getCenter());
+  }
 }
 
 /*?TODO1: optimization: remove the style element when the marker is removed to free memory. See TODO2
   If I decide to remove it, give it an "id" on creation, so I can remove it by "id".
   Only do if necessary (users reporting slow site) and after profiling.
   */
-  function styleMarker (markerPicture) {
-    //Select any element with picSrc and style it
-    let picSrc = markerPicture;
-    let style = document.createElement("style");
-    style.type = "text/css";
-    //Using backtick with picSrc in expression. TODO: Compile with babel
-    //Selecting "*" with picSrc in case google decides to not use divs anymore.
-    style.innerHTML = `
-    * [src="${picSrc}"] {
-      border-radius:30px; 
-      border:1px solid #000 !important; 
-    }`;
-    document.getElementsByTagName("head")[0].appendChild(style);
-  }
+function styleMarker(markerPicture) {
+  //Select any element with picSrc and style it
+  var picSrc = markerPicture;
+  var style = document.createElement("style");
+  style.type = "text/css";
+  //Using backtick with picSrc in expression. TODO: Compile with babel
+  //Selecting "*" with picSrc in case google decides to not use divs anymore.
+  style.innerHTML = "\n    * [src=\"" + picSrc + "\"] {\n      border-radius:30px; \n      border:1px solid #000 !important; \n    }";
+  document.getElementsByTagName("head")[0].appendChild(style);
+}
 
-  //TODO2: See TODO1
-  function removeMarker (markerToRemove) {
-    //Remove the marker from the map: 
-    markerToRemove.setMap(null);
-    //Delete marker:
-    markerToRemove = null;
-  }
+//TODO2: See TODO1
+function removeMarker(markerToRemove) {
+  //Remove the marker from the map: 
+  markerToRemove.setMap(null);
+  //Delete marker:
+  markerToRemove = null;
+}
 
-  //Pass online contact as argument to add marker on map
-  function addMarker (newMarker) {
-    let nmPic = newMarker.picture;
-    let nmName = newMarker.name;
-    let nmID = newMarker.id;
-    let nmUID = newMarker.uniqueID;
-    let nmLatLng = newMarker.latLng;
-    let nmLat = nmLatLng.lat();
-    let nmLng = nmLatLng.lng();
+//Pass online contact as argument to add marker on map
+function addMarker(newMarker) {
+  var nmPic = newMarker.picture;
+  var nmName = newMarker.name;
+  var nmID = newMarker.id;
+  var nmUID = newMarker.uniqueID;
+  var nmLatLng = newMarker.latLng;
+  var nmLat = nmLatLng.lat();
+  var nmLng = nmLatLng.lng();
 
-    styleMarker(nmPic);
-    let thisMarker = new google.maps.Marker({
-      position: nmLatLng,
-      map: map,
-      title: nmName,
-      icon: nmPic,
-      //Makes it a discreet DOM element accessible by css:
-      optimized:false
+  styleMarker(nmPic);
+  var thisMarker = new google.maps.Marker({
+    position: nmLatLng,
+    map: map,
+    title: nmName,
+    icon: nmPic,
+    //Makes it a discreet DOM element accessible by css:
+    optimized: false
+  });
+  //Open chatWindow on click, only if the marker is not yourself
+  if (nmUID != user._id) {
+    thisMarker.addListener("click", function () {
+      openChatWindow(nmName, nmPic, nmID, nmUID, nmLat, nmLng, true);
     });
-    //Open chatWindow on click, only if the marker is not yourself
-    if (nmUID != user._id) {
-      thisMarker.addListener("click", function() {
-        openChatWindow(nmName, nmPic, nmID, nmUID, nmLat, nmLng, true);
-      });
-    }else{
-      //Used to remove marker when erasing location
-      thisMarker.id = "localUserMarker";
-      localUserOnMap = true;
-    };
-    //uniqueID needed to check if the marker is on the map
-    thisMarker.uniqueID = nmUID;
-    markersOnMap.push(thisMarker);
-  }
+  } else {
+    //Used to remove marker when erasing location
+    thisMarker.id = "localUserMarker";
+    localUserOnMap = true;
+  };
+  //uniqueID needed to check if the marker is on the map
+  thisMarker.uniqueID = nmUID;
+  markersOnMap.push(thisMarker);
+}
 
-  //Return Boolean if uniqueID is in inputArray
-  function isUniqueIDInArray (inputArray, uniqueID) {
-    for (let i = inputArray.length - 1; i >= 0; i--) {
-      if (inputArray[i].uniqueID === uniqueID) {
-        return true;
-      }
+//Return Boolean if uniqueID is in inputArray
+function isUniqueIDInArray(inputArray, uniqueID) {
+  for (var i = inputArray.length - 1; i >= 0; i--) {
+    if (inputArray[i].uniqueID === uniqueID) {
+      return true;
     }
-    return false;
   }
+  return false;
+}
 
-  //Add markers if they are not already on the map
-  //Removes them if they are offline
-  function updateMarkersOnMap () {
-    //Add markers
-    for (let i = usersOnline.length - 1; i >= 0; i--) {
-      let newMarker = usersOnline[i];
+//Add markers if they are not already on the map
+//Removes them if they are offline
+function updateMarkersOnMap() {
+  //Add markers
+  for (var i = usersOnline.length - 1; i >= 0; i--) {
+    var newMarker = usersOnline[i];
 
-      //If there are no coordinates, skip it
-      if (! newMarker.lat && ! newMarker.lng) {continue;};
-      //If the marker was already added to the map, skip it
-      if ( isUniqueIDInArray( markersOnMap, newMarker.uniqueID ) ) {continue;};
-      //If it's the localUser's mark, and it's already on the map, skip it.
-      if (localUserOnMap && newMarker.id === sessionID) {continue;};
-      //Otherwise, add the marker
-      newMarker.latLng = new google.maps.LatLng({
-        lat: newMarker.lat, 
-        lng: newMarker.lng
-      });
-      //console.log("Adding");
-      addMarker(newMarker)
+    //If there are no coordinates, skip it
+    if (!newMarker.lat && !newMarker.lng) {
+      continue;
     };
+    //If the marker was already added to the map, skip it
+    if (isUniqueIDInArray(markersOnMap, newMarker.uniqueID)) {
+      continue;
+    };
+    //If it's the localUser's mark, and it's already on the map, skip it.
+    if (localUserOnMap && newMarker.id === sessionID) {
+      continue;
+    };
+    //Otherwise, add the marker
+    newMarker.latLng = new google.maps.LatLng({
+      lat: newMarker.lat,
+      lng: newMarker.lng
+    });
+    //console.log("Adding");
+    addMarker(newMarker);
+  };
 
-    //Remove markers
-    for (let i = markersOnMap.length - 1; i >= 0; i--) {
-      let markerToRemove = markersOnMap[i];
-      //If the marker is not online anymore
-      if ( ! isUniqueIDInArray( usersOnline, markerToRemove.uniqueID ) ) {
-        //console.log("Removing");
-        removeMarker (markerToRemove);
-        //Remove the marker from the array
-        markersOnMap.splice(i, 1);
-      };
+  //Remove markers
+  for (var _i = markersOnMap.length - 1; _i >= 0; _i--) {
+    var markerToRemove = markersOnMap[_i];
+    //If the marker is not online anymore
+    if (!isUniqueIDInArray(usersOnline, markerToRemove.uniqueID)) {
+      //console.log("Removing");
+      removeMarker(markerToRemove);
+      //Remove the marker from the array
+      markersOnMap.splice(_i, 1);
+    };
+  };
+};
+
+/*Remove the local user marker from map, and set coords on usersOnline array to 0.
+  Necessary so that updateMarkersOnMap doesn't add it again on the map
+  immediately after it's removed.*/
+function removeLocalUserMarker() {
+  for (var i = usersOnline.length - 1; i >= 0; i--) {
+    if (usersOnline[i].uniqueID === user._id) {
+      usersOnline[i].lat = 0;
+      usersOnline[i].lng = 0;
+      //console.log("00ing in usersonline");
     };
   };
 
-  /*Remove the local user marker from map, and set coords on usersOnline array to 0.
-    Necessary so that updateMarkersOnMap doesn't add it again on the map
-    immediately after it's removed.*/
-  function removeLocalUserMarker() {
-    for (let i = usersOnline.length - 1; i >= 0; i--) {
-      if (usersOnline[i].uniqueID === user._id) {
-        usersOnline[i].lat = 0;
-        usersOnline[i].lng = 0;
-        //console.log("00ing in usersonline");
-      };
+  for (var _i2 = markersOnMap.length - 1; _i2 >= 0; _i2--) {
+    var markerToRemove = markersOnMap[_i2];
+    if (markerToRemove.id === "localUserMarker") {
+      //console.log("Removing");
+      removeMarker(markerToRemove);
+      //Remove the marker from the array
+      markersOnMap.splice(_i2, 1);
+      localUserOnMap = false;
+      return;
     };
+  };
+}
 
-    for (let i = markersOnMap.length - 1; i >= 0; i--) {
-      let markerToRemove = markersOnMap[i];
-      if ( markerToRemove.id === "localUserMarker" ) {
-        //console.log("Removing");
-        removeMarker (markerToRemove);
-        //Remove the marker from the array
-        markersOnMap.splice(i, 1);
-        localUserOnMap = false;
-        return;
-      };
-    };
-  }
-
-$(document).ready(function(){
+$(document).ready(function () {
   /*When user's coordinates are 0 0, show message explaining how 
   "Broadcast Location" and "Erase Location" work*/
   if (!user.lat && !user.lng) {
-    let broadcastMsg = 'Click "Broadcast Location" to show your position on the map to everyone.<br> \
+    var broadcastMsg = 'Click "Broadcast Location" to show your position on the map to everyone.<br> \
       You can click "Erase Location" to hide your position.';
     if (localLanguage === "Ita") {
       broadcastMsg = 'Clicca su "Trasmetti Posizione" per mostrare a tutti la tua posizione sulla mappa.<br> \
@@ -274,5 +277,6 @@ $(document).ready(function(){
     showMessageOnMap(broadcastMsg);
   };
 
-  let mapUpdateLoop =  setInterval(updateMarkersOnMap, 10000); //10 Seconds
-})
+  updateMarkersOnMap();
+  var mapUpdateLoop = setInterval(updateMarkersOnMap, 10000); //10 Seconds
+});
